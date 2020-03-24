739acd0f4212c5dbb482d395499977515ee017cf
RODRIGO SARMENTO XAVIER <rodrigosarmentoxx@gmail.com>
2020-03-23 23:58:23 -0300

    f

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,43 +19,23 @@ $(function() {
       }
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
-      $.ajax({
-        url: "https://formspree.io/mjvojowv",
-        type: "POST",
-        data: {
-          name: name,
-          phone: phone,
-          email: email,
-          message: message
-        },
-        cache: false,
-        success: function() {
-          // Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        error: function() {
-          // Fail message
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-        },
-        complete: function() {
-          setTimeout(function() {
-            $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-          }, 1000);
-        }
+      //send to formspree
+		$.ajax({
+			url:'https://formspree.io/mjvojowv',
+			method:'POST',
+			data:{
+				name:name,
+				_replyto:email,
+				 email:email,
+				message:message,
+				_subject:'My Form Submission',
+			},
+			dataType:"json",
+			success:function() {
+				console.log('success');	
+				$('#formBlock').hide();
+				$('#thankyouBlock').show();
+			}	
       });
     },
     filter: function() {
